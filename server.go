b7d3d21a0b2e479e91010b086b84f9c54e59d225
Falbue <cyansair05@gmail.com>
2024-11-15 02:21:43 +0500

    создан минимальный сервер

--- server.go
+++ server.go
@@ -1,36 +1,79 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
+    "database/sql"
+    "html/template"
     "log"
     "net/http"
+    _ "github.com/mattn/go-sqlite3" // Импортируем драйвер SQLite
 )
 
-// Обработчик для корневого маршрута "/"
-func index(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/html") // Указываем, что ответ в формате HTML
+// Структура для пользователя
+type User struct {
+    ID       int    `json:"id"`
+    Username string `json:"username"`
+}
 
-    // Чтение содержимого файла index.html
-    content, err := ioutil.ReadFile("templates/index.html")
+// Функция для получения пользователей из базы данных
+func getUsers(db *sql.DB) ([]User, error) {
+    // SQL-запрос для получения id и username всех пользователей
+    query := "SELECT id, username FROM users"
+
+    // Выполнение запроса
+    rows, err := db.Query(query)
     if err != nil {
-        http.Error(w, "Не удалось загрузить страницу", http.StatusInternalServerError)
-        return
+        return nil, err
+    }
+    defer rows.Close()
+
+    var users []User
+    // Чтение строк из результата запроса
+    for rows.Next() {
+        var user User
+        err := rows.Scan(&user.ID, &user.Username)
+        if err != nil {
+            return nil, err
+        }
+        users = append(users, user)
     }
 
-    // Отправка содержимого файла в ответ
-    w.Write(content)
+    // Проверка на ошибки при обходе строк
+    if err := rows.Err(); err != nil {
+        return nil, err
+    }
 
+    return users, nil
 }
 
 func main() {
-    // Назначаем обработчик для маршрута "/"
-    http.HandleFunc("/", index)
-
-    // Запускаем сервер на порту 8080
-    fmt.Println("Сервер запущен на http://localhost:8080")
-    err := http.ListenAndServe(":8080", nil)
+    // Подключение к базе данных SQLite
+    db, err := sql.Open("sqlite3", "../../db_hub/mood_jar.db")
     if err != nil {
-        log.Fatal("Ошибка при запуске сервера: ", err)
+        log.Fatal(err)
     }
+    defer db.Close()
+
+    // Обработка маршрута для главной страницы
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        // Получаем пользователей из базы данных
+        users, err := getUsers(db)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        // Загружаем HTML-шаблон
+        tmpl, err := template.ParseFiles("templates/index.html")
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        // Передаем данные пользователей в шаблон и выводим результат
+        tmpl.Execute(w, users)
+    })
+
+    // Запуск веб-сервера
+    log.Println("Сервер запущен")
+    log.Fatal(http.ListenAndServe(":80", nil))
 }
